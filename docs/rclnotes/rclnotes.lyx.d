09e4197dee514c634a9bdf252a83214b72ed2298
Andrea Censi <andrea@cds.caltech.edu>
2013-03-25 11:34:52 -0700

    done

---
+++ docs/rclnotes/rclnotes.lyx.d
@@ -0,0 +1,2 @@
+rclnotes.tex: rclnotes.lyx /data/work/scm/boot11_env/src/rcl/docs/rclnotes/rclnotes_sec1_intro.lyx
+rclnotes.lyx.d: rclnotes.lyx /data/work/scm/boot11_env/src/rcl/docs/rclnotes/rclnotes_sec1_intro.lyx
