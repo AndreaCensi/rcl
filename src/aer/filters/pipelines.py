7256293702e9abed86f3d12c39d5257d4094da12
Andrea Censi <censi@mit.edu>
2014-11-03 15:50:27 -0500

    minor

--- src/aer/filters/pipelines.py
+++ src/aer/filters/pipelines.py
@@ -1,11 +1,13 @@
+import numpy as np
+import os
+
 from . import aer_raw_relative_timestamp, aer_raw_only_middle
 from ..logs import aer_raw_events_from_file_all
 from aer import logger
 from aer.filters.transitions2 import aer_sign_transitions
 from aer.types import aer_filtered_event_dtype
 from compmake.utils import safe_pickle_dump, safe_pickle_load
-import numpy as np
-import os
+
 
 def aer_pipeline_transitions1(raw_sequence, name):
     """ Iterates over transitions events """
@@ -26,15 +28,15 @@ def aer_pipeline_transitions1_all(filename, name):
         values = aer_pipeline_transitions1_all_slave(filename, name)
         safe_pickle_dump(values, cache_name)
         return values
-    
-    
+
+
 def aer_pipeline_transitions1_all_slave(filename, name):
     """ Returns all events in a numpy array """
     logger.info('Opening file %s' % filename)
     limit = None
     raw_sequence = aer_raw_events_from_file_all(filename, limit=limit)
     filtered = aer_pipeline_transitions1(raw_sequence, name)
-    
+
     # Upper bound on the number
     n = len(raw_sequence)
     logger.info('Allocating %d size' % n)
@@ -45,6 +47,7 @@ def aer_pipeline_transitions1_all_slave(filename, name):
     valid = out[:i]
     return valid
 
+
 def collect_all(sequence):
     logger.info('Reading all events...')
     l = list(sequence)
@@ -53,9 +56,10 @@ def collect_all(sequence):
 
 
 pipelines = {}
-pipelines['p2n'] = lambda seq: aer_sign_transitions(seq, p2n=True, n2p=False) 
+pipelines['p2n'] = lambda seq: aer_sign_transitions(seq, p2n=True, n2p=False)
 pipelines['n2p'] = lambda seq: aer_sign_transitions(seq, p2n=False, n2p=True)
 pipelines['both'] = lambda seq: aer_sign_transitions(seq, p2n=True, n2p=True)
+
 
 def aer_pipeline_choice(raw_sequence, name):
     if name not in pipelines:
--- src/aer/programs/blink_detect/rcl_detect_meat.py
+++ src/aer/programs/blink_detect/rcl_detect_meat.py
@@ -1,29 +1,31 @@
+import itertools
+
+from scipy.ndimage import gaussian_filter
+import numpy as np
+import os
+
 from .config import get_blink_config
 from .fixed_freq_tracker import TrackerFixedFreq
 from aer import aer_load_log_generic, logger
-from aer.filters import aer_pipeline_transitions1, aer_pipeline_transitions1_all
+from aer.filters import aer_pipeline_transitions1, \
+    aer_pipeline_transitions1_all
 from aer.utils import md_argmax
 from aer_led_tracker import AERTrackLogWriter
 from contracts import contract
 from procgraph_pil import imwrite  # XXX
 from reprep import rgb_zoom, scale, posneg
-from scipy.ndimage import gaussian_filter
-import itertools
-import numpy as np
-import os
-
 
 
 class MultipleDetector(object):
     def __init__(self, log, pipeline, sigma, outdir, tracks_filename,
-               interval=None,
-               min_led_distance=0, weight_others=0,
-               detect_smooth_sigma=1.0,
-               detect_neighbors=10, write_png=False):
-        
+                 interval=None,
+                 min_led_distance=0, weight_others=0,
+                 detect_smooth_sigma=1.0,
+                 detect_neighbors=10, write_png=False):
+
         # get configuration
         blink_detect_config = get_blink_config(log)
-        
+
         # create trackers    
         frequencies = blink_detect_config.get_frequencies()
         self.trackers = []
@@ -31,12 +33,12 @@ class MultipleDetector(object):
             others = list(frequencies)
             others.remove(f)
             tracker = TrackerFixedFreq(f, others=others,
-                                          others_weight=weight_others,
-                                          sigma=sigma, interval=interval)
+                                       others_weight=weight_others,
+                                       sigma=sigma, interval=interval)
             self.trackers.append(tracker)
 
         # start track log
-#        tracks_filename = os.path.splitext(log)[0] + '.%s.tracks' % suffix
+        # tracks_filename = os.path.splitext(log)[0] + '.%s.tracks' % suffix
         self.tracklog = AERTrackLogWriter(tracks_filename)
         params = dict(frequencies=frequencies,
                       sigma=sigma, weight_others=weight_others,
@@ -46,7 +48,8 @@ class MultipleDetector(object):
         # open files
         if False:
             raw_sequence = aer_load_log_generic(log)
-            self.transitions = aer_pipeline_transitions1(raw_sequence, pipeline)
+            self.transitions = aer_pipeline_transitions1(raw_sequence,
+                                                         pipeline)
         else:
             self.transitions = aer_pipeline_transitions1_all(log, pipeline)
 
@@ -56,8 +59,8 @@ class MultipleDetector(object):
         self.min_led_distance = int(min_led_distance)
         self.write_png = write_png
         self.outdir = outdir
-            
-    def go(self):    
+
+    def go(self):
         for f in self.transitions:
             # check if this event makes any of those trigger
             triggered = []
@@ -65,14 +68,14 @@ class MultipleDetector(object):
                 res = tracker.integrate(f)
                 if res is not None:
                     triggered.append(tracker)
-                    
+
             # Check that all trackers have produced a frame
             # if so, start two-phase estimation
             if triggered and all([t.has_frame() for t in self.trackers]):
                 self.two_phase_estimation(f['timestamp'], triggered)
-        
+
         self.tracklog.done()
-    
+
     def two_phase_estimation(self, timestamp, triggered):
         # first get the current spots
         first = []
@@ -81,19 +84,20 @@ class MultipleDetector(object):
                              sigma=self.detect_smooth_sigma,
                              neighbors=self.detect_neighbors)
             first.append(dict(tracker=t, detection=td))
-        
+
         # now let's order by quality
         first.sort(key=lambda x: (-x['detection']['quality']))
 
         # one by one
         second = []
-        occupied = [] 
+        occupied = []
         while first:
             f = first.pop(0)
             f_accum = remove_occupied(accum=f['tracker'].get_accum(),
-                                      occupied=occupied, distance=self.min_led_distance)
+                                      occupied=occupied,
+                                      distance=self.min_led_distance)
             f_d = peak_detect(f_accum, sigma=self.detect_smooth_sigma,
-                                        neighbors=self.detect_neighbors)
+                              neighbors=self.detect_neighbors)
             if f_d is None:
                 logger.debug('No track for %s' % f['tracker'])
             else:
@@ -102,49 +106,50 @@ class MultipleDetector(object):
                 second.append(dict(tracker=f['tracker'], detection=f_d))
 
         d = lambda a, b: np.linalg.norm(np.array(a, 'float') - np.array(b))
-        distances = [d(p1, p2) for p1, p2 in itertools.combinations(occupied, 2)]
+        distances = [d(p1, p2) for p1, p2 in
+                     itertools.combinations(occupied, 2)]
         if distances:
             min_distance = np.min(distances)
             assert min_distance >= self.min_led_distance
-        
-        for s in second: 
+
+        for s in second:
             if s['tracker'] in triggered:
                 triggered.remove(s['tracker'])
                 self.output(timestamp, s['tracker'], s['detection'])
-        
+
         # ones that are invalid
         for t in triggered:
             if s['tracker'] == t:
                 s['detection']['centroid'] = (5, 5)
                 self.output(timestamp, s['tracker'], s['detection'])
-        
-                
-    
+
+
     def output(self, timestamp, tracker, detection):
         if self.write_png:
             rcl_detect_write_png(tracker, detection, timestamp, self.outdir)
         else:
-                pass
+            pass
         # print('%14s %s' % (timestamp, tracker.freq))
-        center = detection['centroid'] 
+        center = detection['centroid']
         quality = detection['centroid_nevents']
         id_track = int(tracker.freq)
         self.tracklog.write(timestamp=timestamp, id_track=id_track,
                             x=center[0], y=center[1], quality=quality)
- 
-    
+
+
 def debug_show_list(name, x):
     print(name)
     for s in x:
-        print('- %4d %4d  quality %6.3f  tracker %s' % 
+        print('- %4d %4d  quality %6.3f  tracker %s' %
               (s['detection']['centroid'][0],
                s['detection']['centroid'][1],
                s['detection']['quality'], s['tracker']))
-    
-    
+
+
 @contract(accum='array[HxW]', occupied='list(seq[2](float))', distance='int')
 def remove_occupied(accum, occupied, distance):
-    """ Zeros the elements of the accum in the radius around the given points """
+    """ Zeros the elements of the accum in the radius around the given
+    points """
     accum = accum.copy()
     for x, y in occupied:
         x = int(x)
@@ -152,9 +157,9 @@ def remove_occupied(accum, occupied, distance):
         mask = get_mask(accum, x, y, distance)
         accum[mask] = 0
         assert accum[x, y] == 0
-    return accum    
+    return accum
 
-    
+
 def get_mask(accum, x, y, n):
     H, W = accum.shape
     x0 = max(x - n, 0)
@@ -172,26 +177,28 @@ def get_mask(accum, x, y, n):
     for x in xs:
         mask[x, ys] = True
     return mask
-    
+
+
 @contract(accum='array[HxW]', center='seq[2](int)', neighbors='int,>1')
 def centroid_estimate2(accum, center, neighbors):
     """ Returns S, mi, mj """
     accum = accum.copy()
-    i, j = center[0], center[1] 
+    i, j = center[0], center[1]
     xs = range(max(0, i - neighbors), min(127, i + neighbors))
     ys = range(max(0, j - neighbors), min(127, j + neighbors))
-    
+
     points = [(i, j) for i, j in itertools.product(xs, ys)]
     n = len(points)
     weights = np.array([accum[i, j] for (i, j) in points])
     points = np.array(points).T
     assert points.shape == (2, n)
-    
+
     mi = np.average(points[0, :], weights=weights)
     mj = np.average(points[1, :], weights=weights)
     S = weights.sum()
     return accum, S, mi, mj
-    
+
+
 def peak_detect(accum, sigma, neighbors):
     """ Returns a dictionary with the fields:
     
@@ -209,7 +216,7 @@ def peak_detect(accum, sigma, neighbors):
     # now take a weighted mean around val
     accum_cleared, nevents, mi, mj = \
         centroid_estimate2(accum, center=accum_smooth_max, neighbors=neighbors)
-    
+
     res = {}
     res['accum'] = accum
     res['accum_cleared'] = accum_cleared
@@ -219,14 +226,14 @@ def peak_detect(accum, sigma, neighbors):
     res['centroid_nevents'] = nevents
     res['quality'] = nevents  # overall quality estimate
     return res
-    
-    
+
+
 def rcl_detect_write_png(tracker, detection, timestamp, outdir):
     dirname = os.path.join(outdir, 'f%05d' % tracker.freq)
-    
-    def write_image(name, values):    
+
+    def write_image(name, values):
         time_us = timestamp * 1000 * 1000
-        dirname2 = os.path.join(dirname, name) 
+        dirname2 = os.path.join(dirname, name)
         if not os.path.exists(dirname2):
             os.makedirs(dirname2)
         filename = os.path.join(dirname2, 'ms%08d.png' % time_us)
@@ -239,9 +246,9 @@ def rcl_detect_write_png(tracker, detection, timestamp, outdir):
         logger.debug('print to %r' % filename)
 
     c = detection['centroid']
-    
+
     videos = ['accum', 'accum_cleared', 'accum_smooth']
-    videos = ['accum'] 
+    videos = ['accum']
     for name in videos:
         value = detection[name]
         value[c[0], c[1]] = np.nan
